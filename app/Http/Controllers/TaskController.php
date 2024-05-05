c3d70e44cc384b9acb58e3886f3c7051768229c9
imranpollob <polboy777@gmail.com>
2024-05-05 11:20:34 -0400

    added base controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,35 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class TaskController extends Controller
+class TaskController extends BaseController
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return response()->json(Task::all());
+        $tasks = Task::all();
+
+        return $this->sendResponse($tasks, 'Tasks retrieved successfully.');
     }
 
-    public function store(Request $request)
+
+    public function store(Request $request): JsonResponse
     {
-        $task = Task::create($request->all());
-        return response()->json($task, 201);
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'elapsed_time' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $task = Task::create($input);
+
+        return $this->sendResponse($task, 'Task created successfully.');
     }
 
-    public function show(Task $task)
+
+    public function show($id): JsonResponse
     {
-        return response()->json($task);
+        $task = Task::find($id);
+
+        if (is_null($task)) {
+            return $this->sendError('Task not found.');
+        }
+
+        return $this->sendResponse($task, 'Task retrieved successfully.');
     }
 
-    public function update(Request $request, Task $task)
+
+    public function update(Request $request, Task $task): JsonResponse
     {
-        $task->update($request->all());
-        return response()->json($task, 200);
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $task->name = $input['name'];
+        $task->save();
+
+        return $this->sendResponse($task, 'Task updated successfully.');
     }
 
-    public function destroy(Task $task)
+
+    public function destroy(Task $task): JsonResponse
     {
         $task->delete();
-        return response()->json(null, 204);
+
+        return $this->sendResponse([], 'Task deleted successfully.');
     }
 }
--- database/migrations/2024_05_04_040520_create_tasks_table.php
+++ database/migrations/2024_05_04_040520_create_tasks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->integer('elapsed_time');
             $table->timestamps();
         });

7dba57a7616ae62d4f3d676bfd1f3324e1fb5726
imranpollob <polboy777@gmail.com>
2024-05-06 01:18:05 -0400

    added sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('tasks', TaskController::class);
-Route::post('tasks/{task}/addtime', [TaskController::class, 'addTime']);
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('tasks', TaskController::class);
+  Route::post('tasks/{task}/addtime', [TaskController::class, 'addTime']);
+});
